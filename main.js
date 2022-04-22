e5cbfd4ab929ae2b2b6b18b93ef4b6161ef18280
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-04-22 12:10:53 -0500

    Dibujar Barras: se agregó la funcionalidad para dibujar las barras en el DOM

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
         }
     }
@@ -30,7 +30,7 @@
         this.board.bars.push(this)
         this.kind = "rectangle";
     }
-    self.Board.prototype = {
+    self.Bar.prototype = {
         down: function(){
 
         },
@@ -52,27 +52,33 @@
 
     self.BoardView.prototype = {
         draw: function(){
-            for (var i = board.elements.length-1; i>=0; i==){
-                board.elements[i]
+            for (var i = this.board.elements.length-1; i>=0; i--){
+                var el = this.board.elements[i]
+
+                draw(this.ctx, el);
             }
         }
     }
 
     function draw(ctx, element){
+        if(element != null && element.hasOwnProperty("kind")){
         switch(element.kind){
-            case "square":
+            case "rectangle":
                 ctx.fillRect(element.x, element.y, element.width, element.height);
                 break;
+            }
         }
-        
     }
-})
+})();
 
 window.addEventListener("load", main);
 
 //Controlador
 function main(){
     var board = new Board(800, 400);
+    var bar = new Bar(20,100,40, 100, board)
+    var bar = new Bar(735,100,40, 100, board)
     var canvas = document.getElementById('canvas');
     var board_view = new BoardView(canvas, board);
+    board_view.draw();
 }
